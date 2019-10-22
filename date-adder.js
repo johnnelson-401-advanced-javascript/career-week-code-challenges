0f60bf27aa760c7cf16a3018b1291547e51b2f05
JOHN NELSON <JohnNelson4850@gmail.com>
2019-10-21 23:46:36 -0700

    new approach, still fails Jest

--- date-adder.js
+++ date-adder.js
@@ -1,26 +1,23 @@
 function addDate(date, diff) {
-  let diffAlphaSplit = diff.match('s', 'm', 'h', 'd', 'M', 'y');
-  let diffAlpha = diffAlphaSplit.join('');
-  let diffNumSplit = diff.match(/\d/g);
-  let diffNum = parseInt(diffNumSplit.join(''));
-  console.log('Alpha', diffAlpha, 'Num', diffNum);
-  console.log(date);
-  if(diffAlpha === 's') {
-    return new Date(date.setMilliseconds(diffNum * 1000));
+  let diffNum = parseInt(diff);
+  console.log(diffNum);
+
+  if(diff.match('s') === true) {
+    return new Date(date.getMilliseconds(diffNum * 1000));
   }
-  if(diffAlpha === 'm') {
+  if(diff.match('m') === true) {
     return new Date(date.getMinutes() + diffNum);
   } 
-  if(diffAlpha === 'h') {
+  if(diff.match('h') === true) {
     return new Date(date.getHours() + diffNum);
   }
-  if(diffAlpha === 'd') {
+  if(diff.match('d') === true) {
     return new Date(date.getTime() + diffNum);
   }
-  if(diffAlpha === 'M') {
+  if(diff.match('M') === true) {
     return new Date(date.getMonth() + diffNum);
   }
-  if(diffAlpha === 'y') {
+  if(diff.match('y') === true) {
     return new Date(date.getYear() + diffNum);
   }
 }
