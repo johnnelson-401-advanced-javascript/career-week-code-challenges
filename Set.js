d39f8d42e032aa73ce285a7b46c5554083b7c884
JOHN NELSON <JohnNelson4850@gmail.com>
2019-10-23 16:47:16 -0700

    static methods

--- Set.js
+++ Set.js
@@ -6,6 +6,17 @@ class Set {
     if(!value) { this.push[value]; 
     }
   }
+  has(value) {
+    if(this.set.includes(value)) {
+      return true;
+    }
+  }
+  remove(value) {
+    if(this.set.includes(value)) {
+      this.array.slice(value);
+    }
+  }
+
 }
 
 module.exports = Set;
